16fc7086d2f103f49a3b1143c979e23ca0b59ce5
AdamSkubida <adasiow.s@gmail.com>
2025-02-18 12:53:19 +0100

    Offer feature/ flex-basis added, mobile first implementation

--- src/components/Offer/index.jsx
+++ src/components/Offer/index.jsx
@@ -3,7 +3,14 @@ import css from "./Offer.module.css";
 export const Offer = () => {
   return (
     <>
-      <section className={css["reveal-section"]}>Pierwszy komponent</section>
+      <section className={css["reveal-section"]}>
+        <ul className={css["offer-list"]}>
+          <li className={css["offer-item"]}>1</li>
+          <li className={css["offer-item"]}>2</li>
+          <li className={css["offer-item"]}>3</li>
+          <li className={css["offer-item"]}>4</li>
+        </ul>
+      </section>
       <section className={css["bg-image"]}></section>
       <section className={css["reveal-section"]}>Drugi komponent</section>
     </>
