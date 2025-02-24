621f7fbcca8a2ec367a05de680dc95ea63cd591a
AdamSkubida <adasiow.s@gmail.com>
2025-02-24 16:37:24 +0100

    Offer style/bg-images added, font styles added, flex styles added

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -6,7 +6,7 @@ export const Hero = () => {
         <div className={css.wrapper}>
           <h2 className={css.slogan}>
             Wo<span className={css.dark}>od</span>{" "}
-            <span className={css.light}>Pas</span>ion
+            <span className={css.light}>Des</span>ign
           </h2>
         </div>
       </div>
